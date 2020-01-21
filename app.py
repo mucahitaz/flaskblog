391779e389d39cdb3dd230d97f89ab01c48dce59
mucahitaz <51249755+mucahitaz@users.noreply.github.com>
2020-01-21 19:56:19 +0300

    added login and register template and routes.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b77ece784e27cf7b42137c32fbee5ba4'
 
 posts = [
     {
@@ -28,3 +31,24 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {} !'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "123456":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Failed. Please check username and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
