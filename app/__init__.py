62cee45c810bb4b71a4a290777d5d8d54cfef6a5
mucahitaz <51249755+mucahitaz@users.noreply.github.com>
2020-01-26 13:58:22 +0300

    added validation for email and username.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b77ece784e27cf7b42137c32fbee5ba4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app import routes
